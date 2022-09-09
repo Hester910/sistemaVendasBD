c0de26bd27eae3c2bb40fd651c0d6ab8f2b71475
Giclayne <giclayne@gmail.com>
2022-09-09 18:37:37 -0300

    classe teste

---
+++ src/view/Teste.java
@@ -0,0 +1,29 @@
+package view;
+
+
+public class Teste {
+	
+	public static void main(String[] args) {
+		//CategoriaView c = new CategoriaView();
+		//c.addCategoria();
+		
+		//FornecedorView f = new FornecedorView();
+		//f.addFornecedor();
+		
+		//FuncionarioView func = new FuncionarioView();
+		//func.addFuncionario();
+		
+		
+		//ProdutoView p = new ProdutoView();
+		//p.addProduto();
+		
+		//ItemView i = new ItemView();
+		//i.addItem();
+		
+		VendaView v = new VendaView();
+		v.addVenda();
+		
+	
+	}
+
+}
